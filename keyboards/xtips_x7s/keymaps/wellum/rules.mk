d0df2c70fafbbfb3d47ffe81a8c4f324875b9f68
Ruslan Gainanov <gromrx1@gmail.com>
2025-01-07 14:18:20 +0300

    fix

--- keyboards/xtips_x7s/keymaps/wellum/rules.mk
+++ keyboards/xtips_x7s/keymaps/wellum/rules.mk
@@ -1,7 +1,7 @@
 RGBLIGHT_ENABLE = yes
-VIA_ENABLE = yes
-VIAL_ENABLE = yes
-VIAL_INSECURE = yes
+# VIA_ENABLE = yes
+# VIAL_ENABLE = yes
+# VIAL_INSECURE = yes
 
 SRC += ./oneshot.c
 SRC += ./swapper.c
